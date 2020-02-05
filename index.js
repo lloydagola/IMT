4f70f8d761cdd66bf1e24828027138d0d02ade9f
lloydagola <lloydagola@gmail.com>
2020-02-05 16:23:09 +0300

    Deployment

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = 3000;
 
 
 //set static folder
-app.use(express.static(path.join(__dirname, 'client', 'build')));
+app.use(express.static(path.join(__dirname, 'client', '/build')));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, 'client', "build", 'index.html'));
 });
